5c95815e9707320de8adbeb285e38d419aa59f04
gisma <gisma@users.noreply.github.com>
2023-06-26 08:27:07 +0000

    published by GitHub Actions

---
+++ schedule/schedule-info.js
@@ -0,0 +1,13 @@
+moreaEventData = [
+  {title: "Creating Orthoimages and Point Clouds", url: "/LV-uav-workflow/morea/Postprocessing/assessment.html", start: "2022-06-24T23:59"},
+  {title: "Creating High Quality Products", url: "/LV-uav-workflow/modules/module-postprocessing", start: "2022-06-08", end: "2022-06-22"},
+  {title: "Creating Orthoimages and Point Clouds", url: "/LV-uav-workflow/morea/spatial_analysis/assessment.html", start: "2022-06-24T23:59"},
+  {title: "Analysis of high resolution aerial data", url: "/LV-uav-workflow/modules/module-hr-analysis", start: "2022-06-20", end: "2022-07-14"},
+  {title: "UAV based remote Sensing - A Brief Overview", url: "/LV-uav-workflow/modules/module-introduction", start: "2022-04-20", end: "2021-04-28"},
+  {title: "Getting an Overview of UAV Application in Science", url: "/LV-uav-workflow/morea/introduction/assessment.html", start: "2022-04-28T23:59"},
+  {title: "Get used to scientific UAV applications", url: "/LV-uav-workflow/morea/Hardware_Software/assessment-hs.html", start: "2022-05-04T23:59", end: "2022-05-04T23:59"},
+  {title: "Write a scientific Blog", url: "/LV-uav-workflow/morea/gis-reproducible-research/assessment.html", start: "2022-06-24T23:59"},
+  {title: "Reproducible Research", url: "/LV-uav-workflow/modules/gis-reproducible-research", start: "2022-04-20", end: "2021-07-13"},
+  {title: "Autonomous flights made easy", url: "/LV-uav-workflow/modules/module-fieldwork", start: "2022-05-04", end: "2022-06-01"},
+  {title: "Create a Litchi-Flightplan", url: "/LV-uav-workflow/morea/UAVFieldwork/fw-assessment.html", start: "2022-05-24T23:59"}
+]
